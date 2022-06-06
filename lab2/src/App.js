40396a8227a060424da3ea46440206f761e8e0ea
Zhezhang-Jerry <zhangzhe941020@yahoo.com>
2022-06-05 23:30:26 -0700

    move select an album to right side

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <div style={{color: "white"}}>
       <AlbumInfo coverImg={albums[0].coverImg} name={albums[0].name} tracks={albums[0].tracks} />
-      <h1 style={{textAlign: 'center', fontSize: '120%', padding: '30px'}}>Select an Album</h1>
+      <h1 style={{textAlign: 'right', fontSize: '120%', padding: '30px'}}>Select an Album</h1>
       {
         albums.map( (album) => {
           return <AllAlbum coverImg={album.coverImg} name={album.name} id={album.id}/>
