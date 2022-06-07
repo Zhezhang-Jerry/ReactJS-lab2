d05dd9d78b45611d0556d48faf7cb73fcf566774
Zhezhang-Jerry <zhangzhe941020@yahoo.com>
2022-06-06 22:19:09 -0700

    add state to App.js, but not work yet

--- lab2/src/AllAlbum.js
+++ lab2/src/AllAlbum.js
@@ -1,13 +1,15 @@
-const AllAlbum = (props) => {
+let albumIdArr = [0];
+
+export const AllAlbum = (props) => {
     const albumImageStyle = {borderRadius: '80px', maxWidth: '5%'};
     return (
         <div style={{color: 'white'}}>
             <div style={{display: 'flex'}}>
                 <img src={props.coverImg} alt={props.name} style={albumImageStyle}></img>
-                <p>{props.name}</p>
+                <p onClick={() => {albumIdArr[0] = props.id; console.log(albumIdArr);} }>{props.name}</p>
             </div>
         </div>
     )
 };
 
-export default AllAlbum;
+export { albumIdArr };
--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import albums from './data';
 import AlbumInfo from './AlbumInfo';
-import AllAlbum from './AllAlbum';
-// import { useState } from 'react';
+import { AllAlbum, albumIdArr } from './AllAlbum';
+import { useState } from 'react';
 
 const App = () => {
-
+  const [albumId, setAlbumId] = useState(0)
+  while (!(albumIdArr[0] === albumId)) {
+    setAlbumId(albumIdArr[0])
+  }
   return (
     <div style={{color: "white"}}>
-      <AlbumInfo coverImg={albums[0].coverImg} name={albums[0].name} tracks={albums[0].tracks} />
+      <AlbumInfo coverImg={albums[albumId].coverImg} name={albums[albumId].name} tracks={albums[albumId].tracks} />
       <h1 style={{textAlign: 'right', fontSize: '120%', padding: '30px'}}>Select an Album</h1>
       {
         albums.map( (album) => {
